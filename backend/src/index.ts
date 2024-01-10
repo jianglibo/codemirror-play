6cba3d708bd2373b71b42c2fb77583f4d84e0c72
jianglibo <jianglibo@gmail.com>
2024-01-10 23:09:20 +0800

    Add completion and execute response display
    This commit adds code to display the completion and execute responses on the index.html page. It fetches data from the /completion and /execute endpoints and updates the corresponding div elements with the response data.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,12 +9,25 @@ export default () => {
 
 	app.use(express.static('public'))
 
+	let cache: unknown = null;
+
 	app.get("/completion", async (req: Request, res: Response) => {
-		const response = await fetch('http://example.com');
-		const data = await response.json();
-		res.send({
+		// get the url from environment variable COMPLETION_ENDPOINT
+		const url = process.env.COMPLETION_ENDPOINT ||
+			'http://localhost:4002/rest-get/?obtype=SettingInDb&id=codemirror-play';
+		const response = await fetch(url);
+		if (!cache) {
+			try {
+				cache = await response.json();
+				console.log("fetched: ", cache)
+			} catch (error) {
+				console.log(error)
+				console.log("-------------end of error ----------------")
+			}
+		}
+		res.send(cache || {
 			data: [
-				"free -mh, Display amount of free and used memory in the system",
+				"free -mh, Display amount of free and used memory in the system, local",
 				"df -lh / /mnt/g, report file system disk space usage",
 				"ps aux, report a snapshot of the current processes",
 				"diff -bur folder1 folder2, compare two folders recursively",
@@ -25,7 +38,7 @@ export default () => {
 	app.get("/execute", (req: Request, res: Response) => {
 		let cmd = req.query.cmd as string;
 		let parts = cmd.split(",", 2);
-		shelljs.exec(parts[0], (code: number, stdout: any, stderr: any) => {
+		shelljs.exec(parts[0].trim(), (code: number, stdout: any, stderr: any) => {
 			if (code === 0) {
 				res.send({ data: stdout });
 			} else {
